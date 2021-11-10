044480b1508de784cc0601ece3d3888941dc5f75
Georgeta <ciorbageorgeta@gmail.com>
2021-11-10 22:58:06 +0200

    Add frontend side with working get method

---
+++ MovieTime/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson.Serialization;
+using MovieTime.Helper;
+using MovieTime.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieTime.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieAPI _api = new MovieAPI();
+
+       
+        public async Task<IActionResult> Index()
+        {
+            List<Movie> movie = new List<Movie>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/movie");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                movie = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Movie>>(result);
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieTime/Helper/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieTime.Helper
+{
+    public class MovieAPI
+    {
+        public HttpClient Initial()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9001");
+
+            return Client;
+        }
+    }
+}
